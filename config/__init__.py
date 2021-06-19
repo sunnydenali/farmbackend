5a567a4ccc87de744cc6fe1d66e1753e5052a91d
manikon <manikon@yorku.ca>
2021-06-19 17:44:17 -0400

    Black doesn't format long strings

--- config/__init__.py
+++ config/__init__.py
@@ -4,7 +4,8 @@ from pydantic import BaseSettings
 class CommonSettings(BaseSettings):
     APP_NAME: str = "FARM Intro"
     DEBUG_MODE: bool = True
-    DB_URL = "mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb"
+    DB_URL = "mongodb://127.0.0.1:27017/?compressors=" \
+             "disabled&gssapiServiceName=mongodb"
     DB_NAME = "farmstack"
 
 
