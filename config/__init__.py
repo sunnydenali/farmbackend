e5ca8a8a12a94b58996c2173cca148644298dcd5
sunil <manikon@yorku.ca>
2021-07-10 18:18:53 -0400

    basesettings 3

--- config/__init__.py
+++ config/__init__.py
@@ -14,12 +14,11 @@ class ServerSettings(BaseSettings):
 class DatabaseSettings(BaseSettings):
     DB_URL: str = "mongodb://mongo_user:mongo_password" \
                   "@127.0.0.1:27017/farmstack"
-    DB_NAME: str = ""
+    DB_NAME: str = "farmstack"
 
 
 class Settings(CommonSettings, ServerSettings, DatabaseSettings):
-    class Config:
-        env_file = ".env"
+    pass
 
 
 settings = Settings()
