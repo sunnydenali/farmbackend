8d45e88bd3b69535f195b45d1c32c82ac6c32bfb
sunil <manikon@yorku.ca>
2021-07-10 12:05:41 -0400

    Remove hardcoded mongodb DB_URL

--- config/__init__.py
+++ config/__init__.py
@@ -3,11 +3,7 @@ from pydantic import BaseSettings
 
 class CommonSettings(BaseSettings):
     APP_NAME: str = "FARM Intro"
-    DEBUG_MODE: bool = True
-    DB_URL = "mongodb://mongo_user:mongo_password@mongo:27017"
-    # DB_URL = "mongodb://127.0.0.1:27017/?compressors=" \
-    #          "disabled&gssapiServiceName=mongodb"
-    # DB_NAME = "farmstack"
+    DEBUG_MODE: bool = False
 
 
 class ServerSettings(BaseSettings):
