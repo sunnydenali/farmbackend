2148365436c777d5b046b626367fa93894a1b068
sunil <manikon@yorku.ca>
2021-07-10 12:15:10 -0400

    Port 8000

--- config/__init__.py
+++ config/__init__.py
@@ -8,7 +8,7 @@ class CommonSettings(BaseSettings):
 
 class ServerSettings(BaseSettings):
     HOST: str = "0.0.0.0"
-    PORT: int = 4000
+    PORT: int = 8000
 
 
 class DatabaseSettings(BaseSettings):
